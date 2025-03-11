e21ab43a96752be116719bbeae5c6516041bb8da
Javier Ribó <elribonazo@gmail.com>
2025-03-11 12:12:39 +0100

    fix: improve ci order

--- packages/ridb-core/release.config.js
+++ packages/ridb-core/release.config.js
@@ -12,6 +12,13 @@ export default {
       "v+([0-9])?(.{+([0-9]),x}).x"
     ],
     "plugins": [
+      [
+        "@semantic-release/exec",
+        {
+          "verifyConditionsCmd": "bash -c 'git diff --name-only $(git describe --tags --abbrev=0)..HEAD | grep -v \"package-lock.json\" | grep -v \"CHANGELOG.md\" | grep -v \"docs/\" | grep -v \"packages/ridb/\" | grep -v \"packages/ridb-level/\" | grep -v \"packages/ridb-react/\" | grep -v \"packages/ridb-core/CHANGELOG.md\" || exit 0' ",
+          "prepareCmd": "npx npm-check-updates -u @trust0/ridb -t newest && npx npm-check-updates -u @trust0/ridb-core -t newest && npm i"
+        }
+      ],
       "@semantic-release/changelog",
       [
         "@semantic-release/commit-analyzer",
@@ -23,13 +30,6 @@ export default {
         "@semantic-release/release-notes-generator",
         {
           "preset": "conventionalcommits"
-        }
-      ],
-      [
-        "@semantic-release/exec",
-        {
-          "verifyConditionsCmd": "bash -c 'git diff --name-only $(git describe --tags --abbrev=0)..HEAD | grep -v \"package-lock.json\" | grep -v \"CHANGELOG.md\" | grep -v \"docs/\" | grep -v \"packages/ridb/\" | grep -v \"packages/ridb-level/\" | grep -v \"packages/ridb-react/\" || exit 1' ",
-          "prepareCmd": "npx npm-check-updates -u @trust0/ridb -t newest && npx npm-check-updates -u @trust0/ridb-core -t newest && npm i"
         }
       ],
       "@semantic-release/github",
