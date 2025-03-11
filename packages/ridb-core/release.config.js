4d5532c68a14ef7b64464878a0116bf36f4b8add
Javier Ribó <elribonazo@gmail.com>
2025-03-11 15:19:56 +0100

    fix: testing in browser

--- packages/ridb-core/release.config.js
+++ packages/ridb-core/release.config.js
@@ -7,7 +7,6 @@ export default {
         "@semantic-release/exec",
         {
           "verifyConditionsCmd": "bash -c 'git diff --name-only $(git describe --tags --abbrev=0)..HEAD | grep -v \"package-lock.json\" | grep -v \"CHANGELOG.md\" | grep -v \"docs/\" | grep -v \"packages/ridb/\" | grep -v \"packages/ridb-level/\" | grep -v \"packages/ridb-react/\" | grep -v \"packages/ridb-core/CHANGELOG.md\" || exit 0' ",
-          "prepareCmd": "sh ../../update.sh"
         }
       ],
       ...plugins,
