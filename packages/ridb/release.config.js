abe3b52154921b05816c8d6d58f945e13b3719d5
Javier Ribó <elribonazo@gmail.com>
2025-03-11 15:45:52 +0100

    fix: wrong config

--- packages/ridb/release.config.js
+++ packages/ridb/release.config.js
@@ -1,6 +1,5 @@
-import Config, { branches, plugins } from '../../release.base.js'
+import  { branches, plugins } from '../../release.base.js'
 export default {
-  ...Config,
   tagFormat: '@trust0/ridb@${version}',
   branches: branches,
   plugins: [
