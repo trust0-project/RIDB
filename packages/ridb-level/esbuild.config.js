e3aa965bf8d8624cdfc5f0d60e2c6b1be68c5823
Javier Ribó <elribonazo@gmail.com>
2025-04-23 19:02:02 +0200

    fix: bundle buffer properly from esm cjs

--- packages/ridb-level/esbuild.config.js
+++ packages/ridb-level/esbuild.config.js
@@ -7,7 +7,14 @@ esbuild.build({
     ...generic,
     platform: 'node',
     entryPoints: ['src/index.ts'],
-    external: ['buffer', '@trust0/ridb','@trust0/ridb-core'],
+    external: ['buffer', '@trust0/ridb', '@trust0/ridb-core'],
+    banner: {
+        js: `import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+if (typeof Buffer === 'undefined') {
+    global.Buffer = require('buffer').Buffer;
+}
+`}
 }).catch((err) => {
     console.log(err)
     process.exit(1)
