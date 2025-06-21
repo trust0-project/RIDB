772cf01eb3d5d65d98f59e54a438cd5acae64c89
Javier Ribó <elribonazo@gmail.com>
2025-06-21 18:33:11 +0200

    feat: improve RIDB react

--- packages/ridb-react/src/index.tsx
+++ packages/ridb-react/src/index.tsx
@@ -1,31 +1,44 @@
-import React, { createContext, useMemo, useContext, useEffect, useState } from 'react';
+import React, { createContext, useMemo, useContext, useState, useCallback } from 'react';
 import { DBOptions, RIDB, StartOptions } from "@trust0/ridb";
 import { SchemaTypeRecord } from '@trust0/ridb-core';
 
-type RIDBProps<T extends SchemaTypeRecord> = DBOptions<T> & {startOptions?: StartOptions<T>};
+export type DatabaseState = 'disconnected' | 'loading' | 'loaded' | 'error';
+type Context<T extends SchemaTypeRecord> = {
+  db: RIDB<T>;
+  state: DatabaseState;
+  start: (options: StartOptions<T>) => Promise<void>;
+  stop: () => Promise<void>;
+} | null
 
 
-const RIDBContext = createContext<RIDB<any> | null>(null);
-
-export type RIDBComponentProps<T extends SchemaTypeRecord> = RIDBProps<T> & {
-  children?: React.ReactNode;
-};
+const RIDBContext = createContext<Context<any>>(null);
 
 // Hook that returns the database and loading state
 export function useRIDB<T extends SchemaTypeRecord>() {
-  const context = useContext(RIDBContext);
+  const context = useContext<Context<T>>(RIDBContext);
   if (!context) {
     throw new Error('useRIDB must be used within a RIDB provider');
   }
-  return context as RIDB<T>;
+  return context
 }
 
-
-export function RIDBDatabase<T extends SchemaTypeRecord>({ children, ...props }: RIDBComponentProps<T>) {
-  const dbInit = props as RIDBProps<T>;
+export function RIDBDatabase<T extends SchemaTypeRecord>({ children, ...props }:  DBOptions<T> & {startOptions?: StartOptions<T>}  & {
+  children?: React.ReactNode;
+}) {
+  const dbInit = props as DBOptions<T>;
   const db = useMemo(() => new RIDB<T>(dbInit), []);
+  const start = useCallback(async (options: StartOptions<T>) => {
+      setState('loading');
+      await db.start(options);
+      setState('loaded');
+  }, [db]);
+  const stop = useCallback(async () => {
+    setState('disconnected');
+    await db.close();
+  }, [db]);
+  const [state, setState] = useState<DatabaseState>('disconnected');
   return (
-    <RIDBContext.Provider value={db}>
+    <RIDBContext.Provider value={{db, state, start, stop}}>
       {children}
     </RIDBContext.Provider>
   );
